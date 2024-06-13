5d333537569f3f1b1f9a291411f1dadf11e33c72
Bablukanna6 <Bablukanna6@gmail.com>
2024-06-13 11:37:37 +0530

    Update image.js

--- js/image.js
+++ js/image.js
@@ -3,7 +3,7 @@ const crousalImage = [
   "https://zenfolio.creatorcdn.com/39a5f814-91f3-423e-a808-d8db771af4d9/0/1/0/X3XL/0-0-0/4d0ba965-d111-4314-8e67-bf895fc7036a/1/1/SID01.jpg?fjkss=exp=1723442400~hmac=73870060fd92feb7294c5d8cc212050dfbcdb581226f089a019d089862c356a2",
  "https://zenfolio.creatorcdn.com/39a5f814-91f3-423e-a808-d8db771af4d9/0/1/0/X3XL/0-0-0/f1a035ce-5616-48b8-ab61-fea6b412f67d/1/1/SID03.jpg?fjkss=exp=1723442400~hmac=73870060fd92feb7294c5d8cc212050dfbcdb581226f089a019d089862c356a2",
  "https://zenfolio.creatorcdn.com/39a5f814-91f3-423e-a808-d8db771af4d9/0/1/0/X3XL/0-0-0/7eb5c472-5eed-4733-af4c-f912970777fd/1/1/SID04.jpg?fjkss=exp=1723442400~hmac=73870060fd92feb7294c5d8cc212050dfbcdb581226f089a019d089862c356a2",
-  "https://images.squarespace-cdn.com/content/v1/58590703e4fcb586c80e4914/1486224613955-MAXD4YS5OZSCWOGUQ6VH/Brij+holi%2C+Mumbai+A7s.jpg?format=2500w",
+  "https://zenfolio.creatorcdn.com/39a5f814-91f3-423e-a808-d8db771af4d9/0/1/0/X3XL/0-0-0/4052f7dc-607e-412f-9713-95777f2f99b4/1/1/SID07.jpg?fjkss=exp=1723442400~hmac=73870060fd92feb7294c5d8cc212050dfbcdb581226f089a019d089862c356a2",
   "https://zenfolio.creatorcdn.com/cb2303e7-3d21-4600-b216-d39b6ec9ef0d/0/1/0/X3XL/0-0-0/3575a530-f0e7-44b8-8815-1dad0fa2dcbb/1/1/SID03449.jpg?fjkss=exp=1720882800~hmac=c5b103b515c37fd12eeddc1836fffd6e9db9cc64e4a7b01725985688ae88c304&quot"
 
 ];
