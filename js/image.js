b0f26b4da37c3c593733761586f0858fe08ecae4
Bablukanna6 <Bablukanna6@gmail.com>
2024-06-13 11:48:34 +0530

    Update image.js

--- js/image.js
+++ js/image.js
@@ -12,7 +12,7 @@ const crousalImage = [
 "https://zenfolio.creatorcdn.com/39a5f814-91f3-423e-a808-d8db771af4d9/0/1/0/X3XL/0-0-0/e2fa604b-05f0-4860-8ebb-64dbe3e16647/1/1/SID013.jpg?fjkss=exp=1723442400~hmac=73870060fd92feb7294c5d8cc212050dfbcdb581226f089a019d089862c356a2",
 "https://zenfolio.creatorcdn.com/39a5f814-91f3-423e-a808-d8db771af4d9/0/1/0/X3XL/0-0-0/39527189-5431-400a-b074-352aa340d736/1/1/SID011.jpg?fjkss=exp=1723442400~hmac=73870060fd92feb7294c5d8cc212050dfbcdb581226f089a019d089862c356a2", 
 "https://zenfolio.creatorcdn.com/39a5f814-91f3-423e-a808-d8db771af4d9/0/1/0/X3XL/0-0-0/d8020210-b147-45b3-977b-a62238acde52/1/1/SID015.jpg?fjkss=exp=1723442400~hmac=73870060fd92feb7294c5d8cc212050dfbcdb581226f089a019d089862c356a2",
-
+"https://zenfolio.creatorcdn.com/39a5f814-91f3-423e-a808-d8db771af4d9/0/1/0/X3XL/0-0-0/dbd19f20-0a5b-41de-8996-bd2e52d57cea/1/1/SID016.jpg?fjkss=exp=1723442400~hmac=73870060fd92feb7294c5d8cc212050dfbcdb581226f089a019d089862c356a2",
 
 
 
